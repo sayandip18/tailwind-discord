826ed573eece66f4785e6ce6032ac7496ed014f8
Sayandip Halder <sayandiph4@gmail.com>
2022-01-07 14:33:26 +0530

    basic setup of dashboard

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
+import { FaFire, FaOldRepublic } from 'react-icons/fa'
+
+const SideBar = () => {
+    return (
+        <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col
+        bg-gray-900 text-white shadow-lg">
+            <SideBarIcon icon={<FaFire size="28" />} />
+            <SideBarIcon icon={<BsPlus size="32" />} />
+            <SideBarIcon icon={<BsFillLightningFill size="20" />} />
+            <SideBarIcon icon={<FaOldRepublic size="20" />} />
+            <SideBarIcon icon={<BsGearFill size="22" />} />
+        </div>
+    )
+};
+
+const SideBarIcon = ({icon}) => {
+    return (
+        <div className="sidebar-icon">
+            {icon}
+        </div>
+    )
+}
+
+export default SideBar;
